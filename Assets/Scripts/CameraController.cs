015b4b5efda8cb7ad506269af344275d1c158838
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2019-10-30 10:13:06 +0100

    Moving the Camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 vector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vector = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + vector;
+    }
+}
